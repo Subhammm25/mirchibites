87a7280cb4ba94e82d4bd3f7e512c7a2bd7edabc
Subham <subham252003@gmail.com>
2024-10-05 19:07:59 +0530

    Fix: Import dj_database_url

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import firebase_admin
 from firebase_admin import credentials
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
